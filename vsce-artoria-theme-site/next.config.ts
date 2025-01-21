2bcdb6e25fdd11b494270828b6e3ec30726a2243
Anh Nguyen <anhnguyen@aaanh.com>
2025-01-21 00:05:11 -0500

    Fixing CI

--- vsce-artoria-theme-site/next.config.ts
+++ vsce-artoria-theme-site/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
